790ab5b9795a77b0e5e1165b32a4216b260b3115
laserda <komanatsefabriceigor@gmail.com>
2025-11-11 14:13:04 -0500

    updagrad

--- Services/MenuService/MenuService.Application/DependencyInjection.cs
+++ Services/MenuService/MenuService.Application/DependencyInjection.cs
@@ -16,15 +16,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices
         (this IServiceCollection services, IConfiguration configuration)
     {
-        // TODO : Mettre dans le building block
-        services.AddMediatR(config =>
-        {
-            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-        });
 
-        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddValidationBehaviors(configuration);
 
         return services;
     }
