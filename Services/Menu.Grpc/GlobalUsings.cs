bb9d4ab445c741ba4e2df66f80c161334772d63c
laserda <komanatsefabriceigor@gmail.com>
2025-11-11 10:16:10 -0500

    suppresson de projet non utile

--- Services/Menu.Grpc/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using Microsoft.EntityFrameworkCore;
-global using Menu.Grpc.Data;
-global using Menu.Grpc.Data.Extensions;
-global using Menu.Grpc.Services;
-global using IDR.Library.BuildingBlocks;
-global using IDR.Library.BuildingBlocks.Interceptors;
