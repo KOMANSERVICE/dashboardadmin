517ca5375b972b53e8f6afc549990c563e5edba4
laserda <komanatsefabriceigor@gmail.com>
2025-12-06 19:55:05 -0500

    fix

--- scripts/Start-DashBoardAdminAgent.ps1
+++ scripts/Start-DashBoardAdminAgent.ps1
@@ -149,10 +149,13 @@ function Move-IssueToColumn {
         
         # Essayer d'abord avec organization (plus courant pour les entreprises)
         $projectId = $null
-        $orgQuery = @"
-query { organization(login: "$Owner") { projectV2(number: $ProjectNumber) { id } } }
-"@
-        $orgResult = gh api graphql -f query="$orgQuery" 2>$null
+        
+        # Utiliser les variables GraphQL pour eviter les problemes d'echappement
+        $orgResult = gh api graphql `
+            -f query='query($login: String!, $number: Int!) { organization(login: $login) { projectV2(number: $number) { id } } }' `
+            -f login="$Owner" `
+            -F number=$ProjectNumber 2>$null
+        
         if ($orgResult) {
             $orgData = $orgResult | ConvertFrom-Json
             if ($orgData.data.organization.projectV2) {
@@ -163,10 +166,11 @@ query { organization(login: "$Owner") { projectV2(number: $ProjectNumber) { id }
         
         # Si pas trouve, essayer avec user
         if (-not $projectId) {
-            $userQuery = @"
-query { user(login: "$Owner") { projectV2(number: $ProjectNumber) { id } } }
-"@
-            $userResult = gh api graphql -f query="$userQuery" 2>$null
+            $userResult = gh api graphql `
+                -f query='query($login: String!, $number: Int!) { user(login: $login) { projectV2(number: $number) { id } } }' `
+                -f login="$Owner" `
+                -F number=$ProjectNumber 2>$null
+            
             if ($userResult) {
                 $userData = $userResult | ConvertFrom-Json
                 if ($userData.data.user.projectV2) {
@@ -182,24 +186,10 @@ query { user(login: "$Owner") { projectV2(number: $ProjectNumber) { id } } }
         }
         
         # Obtenir le field ID pour Status et ses options
-        $fieldQuery = @"
-query {
-    node(id: "$projectId") {
-        ... on ProjectV2 {
-            field(name: "Status") {
-                ... on ProjectV2SingleSelectField {
-                    id
-                    options {
-                        id
-                        name
-                    }
-                }
-            }
-        }
-    }
-}
-"@
-        $fieldResult = gh api graphql -f query="$fieldQuery" 2>$null
+        $fieldResult = gh api graphql `
+            -f query='query($nodeId: ID!) { node(id: $nodeId) { ... on ProjectV2 { field(name: "Status") { ... on ProjectV2SingleSelectField { id options { id name } } } } } }' `
+            -f nodeId="$projectId" 2>$null
+        
         if (-not $fieldResult) {
             Write-Host "   [ERROR] Impossible de recuperer le champ Status" -ForegroundColor Red
             return $false
@@ -209,7 +199,6 @@ query {
         
         if (-not $fieldData.data.node.field) {
             Write-Host "   [ERROR] Champ 'Status' non trouve dans le project" -ForegroundColor Red
-            Write-Host "   [DEBUG] Reponse: $fieldResult" -ForegroundColor DarkGray
             return $false
         }
         
@@ -231,21 +220,12 @@ query {
         Write-Host "   [INFO] Option trouvee: $($targetOption.name) (ID: $($targetOption.id))" -ForegroundColor DarkGray
         
         # Executer la mutation pour deplacer l'item
-        $mutation = @"
-mutation {
-    updateProjectV2ItemFieldValue(input: {
-        projectId: "$projectId"
-        itemId: "$($item.id)"
-        fieldId: "$statusFieldId"
-        value: { singleSelectOptionId: "$($targetOption.id)" }
-    }) {
-        projectV2Item {
-            id
-        }
-    }
-}
-"@
-        $mutationResult = gh api graphql -f query="$mutation" 2>&1
+        $mutationResult = gh api graphql `
+            -f query='mutation($projectId: ID!, $itemId: ID!, $fieldId: ID!, $optionId: String!) { updateProjectV2ItemFieldValue(input: { projectId: $projectId, itemId: $itemId, fieldId: $fieldId, value: { singleSelectOptionId: $optionId } }) { projectV2Item { id } } }' `
+            -f projectId="$projectId" `
+            -f itemId="$($item.id)" `
+            -f fieldId="$statusFieldId" `
+            -f optionId="$($targetOption.id)" 2>&1
         
         if ($LASTEXITCODE -ne 0) {
             Write-Host "   [ERROR] Echec de la mutation: $mutationResult" -ForegroundColor Red
